49cb833dcccd447e134e15f15f645f55af13b1e8
copipaste <jhoeldebrayquispe@gmail.com>
2023-11-25 01:33:02 -0400

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _username = '';
+  String _password = '';
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // Implementar la lógica de inicio de sesión aquí
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Username'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your username';
+                }
+                return null;
+              },
+              onSaved: (value) => _username = value!,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+              onSaved: (value) => _password = value!,
+            ),
+            ElevatedButton(
+              onPressed: _submitForm,
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
